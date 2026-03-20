1bc41cc9ce9b24eebce4f70e677f21b6db34ed75
nandinisaha13 <nandinisaha13@gmail.com>
2026-03-20 23:25:24 +0530

    Refactor BaseTest to use ChromeOptions for headless browser setup and improve code formatting

--- src/test/java/com/nandini/automation/base/BaseTest.java
+++ src/test/java/com/nandini/automation/base/BaseTest.java
@@ -2,6 +2,7 @@ package com.nandini.automation.base;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Listeners;
@@ -9,27 +10,32 @@ import org.testng.annotations.Listeners;
 import com.nandini.automation.utils.ConfigReader;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+
 @Listeners(com.nandini.automation.utils.TestListener.class)
 public class BaseTest {
+
     public static WebDriver driver;
 
     @BeforeMethod
-    
-    public void setup()
-    {
+    public void setup() {
+
         ConfigReader config = new ConfigReader();
         WebDriverManager.chromedriver().setup();
-        driver= new ChromeDriver();
-
+        ChromeOptions options = new ChromeOptions();
+        if (System.getProperty("headless") != null) {
+            options.addArguments("--headless=new");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
+        }
+        driver = new ChromeDriver(options); 
         driver.manage().window().maximize();
         driver.get(config.getProperty("baseUrl"));
-
     }
 
     @AfterMethod
-    public void teardown()
-    {
-        driver.quit();
+    public void teardown() {
+        if (driver != null) {
+            driver.quit();
+        }
     }
-
 }
